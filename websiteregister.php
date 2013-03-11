63857525478a149aa0fabd4500112b57875a7aff
unknown <ajinkya.salve01@gmail.com>
2013-03-11 20:27:05 +0530

    Fixed the insertion query bug, cleaning up code

--- websiteregister.php
+++ websiteregister.php
@@ -10,9 +10,6 @@ if(!empty($_POST['tb_wsname']) && !empty($_POST['tb_wsurl']))
     $wsname = mysql_real_escape_string($_POST['tb_wsname']);
     $wsurl = mysql_real_escape_string($_POST['tb_wsurl']);
     
-     echo $wsname;
-	 echo $wsurl;
-	 
      $query = " INSERT INTO master_websites (`ws_id`, `ws_name`, `ws_url`) VALUES (NULL, '$wsname', '$wsurl') ";
      $result=mysql_query($query,$connection);
      
