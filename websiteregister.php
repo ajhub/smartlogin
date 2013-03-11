34f269631e36e8043c61c436e0f37d92b4dbba8d
unknown <ajinkya.salve01@gmail.com>
2013-03-11 20:03:56 +0530

    Changed the insertion query.

--- websiteregister.php
+++ websiteregister.php
@@ -9,9 +9,9 @@ if(!empty($_POST['tb_wsname']) && !empty($_POST['tb_wsurl']))
 
     $wsname = mysql_real_escape_string($_POST['tb_wsname']);
     $wsurl = mysql_real_escape_string($_POST['tb_wsurl']);
+    
      
-     
-     $query = " INSERT INTO master_websites ('ws_name','ws_url') VALUES ('$wsname','$wsurl') ";
+     $query = " INSERT INTO master_websites ('ws_id','ws_name','ws_url') VALUES (NULL,'$wsname','$wsurl') ";
      $result=mysql_query($query,$connection);
      
 	 if(!$result)
