220ab43e0648d7d766ec71ee77f141834c9989b5
ajhub <ajinkya.salve01@gmail.com>
2018-02-15 02:17:51 +0530

    Update documentation.php

--- documentation.php
+++ documentation.php
@@ -11,7 +11,7 @@ You'l be briefed in the Documentation section about the details.
    Just need to change some values, so that the scripts can acceess your websites database. 
    You'l get it as you go through the config.php file.
    
-4. Whom do you trust the most on this earth.? I know you must thinking about yourself , pretty boostful but fine with us.
+4. Whom do you trust the most? We know the answer! Yourself , pretty boostful but fine with us.
    So go ahead, place your login mechanism or watever you know it as ,in <loginmech.php>. You'l be briefed more about it in the file itself.
    
 5. Almost done. We said it was easy.
@@ -19,5 +19,5 @@ You'l be briefed in the Documentation section about the details.
 
 6. Register your website with us so that we can update the mobile app with your data.
 
-7. Now delete all the data your website has, and we mean all everything, all tables, all files. 
+7. Now delete all the data your website has, and we mean everything, all tables, all files. 
 Ok stop abusing , kidding, you are all ready to go. Your users will praise you for protecting them from those inhuman keyloggers.    
